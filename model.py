06c9bf5f6a961f8c6d6754cea6e75073f7da3912
AhmedAymaan <78112105+AhmedAymaan@users.noreply.github.com>
2023-07-10 22:58:02 +0300

    Update model.py

--- model.py
+++ model.py
@@ -3,18 +3,9 @@ from keras.models import load_model
 from keras.utils import img_to_array
 import numpy as np
 from PIL import Image
-import h5py
-import gdown
 
-# URL of the publicly accessible .h5 file
-file_url = "https://drive.google.com/uc?export=download&id=1LKoxag5ORvsiGcDjn_mziNH7tMHk1fDJ"
-
-# Download the file and save it locally as 'model.h5'
-output_file = 'lungcancer.h5'
-gdown.download(file_url, output_file, quiet=False)
-
-# Load the .h5 file using h5py
-model = h5py.File(output_file, 'r')
+# Loading model
+model = load_model("lungcancer.h5")
 
 # Preparing and pre-processing the image
 def preprocess_img(img_path):
